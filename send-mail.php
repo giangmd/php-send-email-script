a58c9c307c9bd5e0b38fafa04bec711d63788b0d
giangmd <giangmd@soft-flight.work>
2019-04-12 16:35:44 +0700

    Initial source code

---
+++ send-mail.php
@@ -0,0 +1,30 @@
+<?php
+// Pear Mail Library
+require_once "./Mail/Mail.php";
+
+$from = '<giang@gmail.com>';
+$to = '<giangmd93@gmail.com>';
+$subject = 'Hi!';
+$body = "Hi,\n\nHow are you?";
+
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '465',
+        'auth' => true,
+        'username' => 'giang.smtp.dev@gmail.com',
+        'password' => 'nnlfubvbdicsbqwx'
+    ));
+
+$mail = $smtp->send($to, $headers, $body);
+
+if (PEAR::isError($mail)) {
+    echo('<p>' . $mail->getMessage() . '</p>');
+} else {
+    echo('<p>Message successfully sent!</p>');
+}
